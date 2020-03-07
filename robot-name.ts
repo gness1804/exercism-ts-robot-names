ce774b143a7108a166aaf7a57392e96f9ef65d9a
gness1804 <graham.nessler@gmail.com>
2020-03-07 10:55:55 -0600

    First test passing

--- robot-name.ts
+++ robot-name.ts
@@ -6,10 +6,29 @@ export default class RobotName {
   }
 
   generateName(): string {
-    return 'test';
+    const str1 = this.generateRandomLetter();
+    let str2 = this.generateRandomLetter();
+
+    let nums = [
+      this.generateRandomDigit(),
+      this.generateRandomDigit(),
+      this.generateRandomDigit()
+    ];
+
+    // enforce that the letters are not within 2 or less of each other
+    if (Math.abs(str1.charCodeAt(0) - str2.charCodeAt(0)) <= 2)
+      this.generateName();
+    return `${str1}${str2}${nums.join(" ").replace(/\s/g, "")}`;
   }
 
-  resetName(): void {
+  resetName(): void {}
 
+  generateRandomLetter(): string {
+    // ASCII codes for A-Z: 65-90
+    return String.fromCharCode(Math.random() * (91 - 65) + 65);
+  }
+
+  generateRandomDigit(): number {
+    return Math.floor(Math.random() * (10 - 0) + 0);
   }
 }
